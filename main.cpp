8830788315bbff6d4c6cc10de59b31bd4bd0359f
KyawBhoneNaing <kyawbnaing@gmail.com>
2025-09-18 02:57:28 +0000

    Salesperson

--- main.cpp
+++ main.cpp
@@ -1,5 +1,60 @@
 #include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+using namespace std;
 
-int main() {
-  std::cout << "Hello World!\n";
+int main()
+{
+
+    string firstName, lastName;
+    double baseSalary, commissionPercent;
+    double totalSales, expenses;
+    double commission, totalPay;
+
+    string fileName;
+    ifstream inFile;
+
+
+    cout << "Enter the name of the input file: ";
+    cin >> fileName;
+
+
+    inFile.open(fileName.c_str());
+
+    if (!inFile)
+    {
+        cout << "Error: Cannot open file." << endl;
+        return 1;
+    }
+
+
+    inFile >> firstName >> lastName;
+    inFile >> baseSalary >> commissionPercent;
+    inFile >> totalSales;
+    inFile >> expenses;
+
+    inFile.close();
+
+
+    commission = (commissionPercent / 100) * totalSales;
+    totalPay = baseSalary + commission;   
+
+
+    cout << fixed << setprecision(2);
+    cout << "\nPayroll data for " << firstName << " " << lastName << endl;
+    cout << endl;
+
+    cout << "Base Salary:" << setw(10) << baseSalary << endl;
+
+    cout << "Commission:" << setw(10) << commission 
+         << " (" << fixed << setprecision(1) << commissionPercent 
+         << "% of " << setprecision(2) << totalSales << ")" << endl;
+
+    cout << "Expenses:" << setw(12) << expenses << endl;
+    cout << "---------" << endl;
+
+    cout << "Total:" << setw(14) << totalPay << endl;
+
+    return 0;
 }
